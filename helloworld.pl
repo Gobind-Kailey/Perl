3aa78f45064ac88e67e1923d6e2cb195591faeda
kaileg2 <gobind2975@gmail.com>
2025-10-25 15:43:45 -0400

    Basic Syntax

--- helloworld.pl
+++ helloworld.pl
@@ -1,2 +1,31 @@
+# Use perl to execute this file
 #!/usr/bin/perl
+ 
+# Directives to help catch errors
+# Prevents un-declared variables 
+use strict; 
+# Helps catch potential errors
+use warnings; 
+
+# Statements
 print "Hello, world!\n";
+
+=Start
+print "Hello, world!\n";
+print "Hello, world!\n";
+print "Hello, world!\n";
+=cut
+
+my $name = "Gobind"; 
+my $Name = "Kailey"; 
+
+print "$name\n";
+print "$Name\n"; 
+# print "$NAME\n";  # This will cause a compile-time error due to 'use strict'
+
+if(1 == 1){
+
+    # This line of code will still execute even without the ; 
+    print "This is conditional\n"; 
+}
+

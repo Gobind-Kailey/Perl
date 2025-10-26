cfd0403efcb5859aa924b4548c8dce61893a529a
kaileg2 <gobind2975@gmail.com>
2025-10-26 15:19:48 -0400

    Updating Section 3 + 4

---
+++ Section_4/sub_routine.pl
@@ -0,0 +1,15 @@
+# with this line of code are we letting the system know that we are usign the perl \interpreter?
+#!/usr/bin/perl 
+
+
+use strict; 
+use warnings; 
+use feature 'say'; 
+
+sub greet{
+
+    say "Welcome to Perl Programming"; 
+}
+
+great(); 
+

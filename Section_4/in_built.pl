a3931a9618e9dd4d87631b70b45e76b7f73c7daf
kaileg2 <gobind2975@gmail.com>
2025-10-26 16:21:51 -0400

    In built functions

--- Section_4/in_built.pl
+++ Section_4/in_built.pl
@@ -14,4 +14,18 @@ my $string_name = "Gobind";
 my $len = 
 say "length = ". length($string_name); 
 
+# Will print any number 0 - 10, float 
+say rand(10); 
+say sqrt(25); # 5
+
+
+# adding elements to array
+
+my @fruits = ("apple", "Banana");
+push (@fruits, "Cherry"); 
+print "@fruits"; 
+
+# Files
+
+# Opening Files
 

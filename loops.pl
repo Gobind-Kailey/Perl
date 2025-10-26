a311bbb27b39f22a78684474aef13a1316fa0679
kaileg2 <gobind2975@gmail.com>
2025-10-25 20:44:32 -0400

    Loops

--- loops.pl
+++ loops.pl
@@ -5,3 +5,22 @@ use strict;
 use warnings;
 
 
+# For each loop
+my @numbers = (1,8,3,4,5);
+
+foreach my $num (@numbers) {
+
+    print $num. "\n"; # This doesn't work unless you use "" 
+}
+
+
+# Unless loop
+
+my $count = 1; 
+
+unless($count != 1)
+{
+    print "Count = $count"; 
+    $count++; 
+
+}

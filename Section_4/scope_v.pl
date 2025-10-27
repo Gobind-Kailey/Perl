5e444918eb9732a25f90f679d80e89417b290344
kaileg2 <gobind2975@gmail.com>
2025-10-26 21:19:20 -0400

    Scope

---
+++ Section_4/scope_v.pl
@@ -0,0 +1,20 @@
+
+#! usr/bin/perl
+
+
+use strict;
+use warnings; 
+use feature 'say'; 
+
+# Perl has 3 scopes
+
+=Start 
+
+#1 = Global scope (our)
+#2 = Lexical Scope (my) - local
+#3 = Persistent scope (state) - like strict in C, retains value across multiple
+                                function calls. 
+
+
+You can use my as a global aswell, if it is declared outside already
+=cut 

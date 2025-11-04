b6987ee4cb389e57358ff44000e3fdc8ec8f2992
kaileg2 <gobind2975@gmail.com>
2025-11-03 20:20:54 -0500

    Pushing regex

---
+++ AMD_ASIC_Prep/file IO Foundations/Regex and Parsing/regex.pl
@@ -0,0 +1,32 @@
+
+
+#!usr/bin/perl 
+
+
+use strict; 
+use warnings; 
+use feature 'say'; 
+
+open(my $fh, '<', 'timing.log') or die "Couldn't open file";
+
+# This checks if the next line exists and while it does it will keep running 
+# while(my $line = <$fh>)
+# {
+#     print $line; 
+# }
+my $minimum = 9999; 
+
+while(my $line = <$fh>)
+{
+
+    if ($line =~ /Slack\s*=\s*(-?\d+\.\d+)/)
+    {
+        # This only takes the values that are in the brackets 
+        my $slack = $1;
+        say $slack; 
+        if($slack < $minimum ){ $minimum = $slack;}
+    }
+}
+
+say "Smallest value = $minimum"; 
+
